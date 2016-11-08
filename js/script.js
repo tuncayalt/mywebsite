3a5da8b123e7a8f6639e1627c83a951cf4bdfb1c
Tuncay Altinpulluk <tuncayalt@gmail.com>
2016-11-08 23:03:59 +0300

    parallax and new jumbo

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $(".navbar-toggle").blur(function(event){
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768){
-      $("#bs-example-navbar-collapse-1").collapse('hide');
-    }
-  });
-});
-
