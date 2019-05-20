9cc2fb7bb1637726b4d9cc964a04f0a6c53c40e6
tuncay <tuncayalt@gmail.com>
2019-05-20 12:09:13 -0400

    added parallax and font awesome social media icons

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ $(function(){
 		$("#bs-navbar-collapse-1").collapse('hide');
 	});
 });
+
+//PARALLAX
+$(document).ready(function() {
+
+    "use strict";
+    
+    $(window).bind('load', function() {
+        "use strict";
+        parallaxInit();
+    });
+
+    function parallaxInit() {
+        "use strict";
+        $('#img-jumbo').parallax("30%", 0.1);
+        /*add as necessary*/
+    }
+});
